2409d0f6f954134baaa5bd4cb00df93281b9c4d8
quocthainguyenle14 <87459995+quocthainguyenle14@users.noreply.github.com>
2024-05-12 23:57:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 function ToggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
     icon.classList.toggle("open")
+=======
+function ToggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+>>>>>>> 01293efe0d85f025c5b228315cd6cf96115d2a78
 }
